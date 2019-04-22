1439dfd7468d0c1e7d0edaecbd5030276fb2ec26
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 14:07:26 -0400

    test the movement api controller returns hello workd

--- src/test/java/com/tddapps/rt/api/controllers/MovementTest.java
+++ src/test/java/com/tddapps/rt/api/controllers/MovementTest.java
@@ -1,12 +1,34 @@
 package com.tddapps.rt.api.controllers;
 
+import com.tddapps.rt.model.SettingsReader;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(Movement.class)
 public class MovementTest {
+    @Autowired
+    private MockMvc web;
+
+    @MockBean
+    private SettingsReader settingsReaderMock;
+
     @Test
-    public void CanRunTests(){
-        assertTrue(true);
+    public void ReturnsHelloWorld() throws Exception {
+        web.perform(get("/api/movement"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World!")));
     }
 }
