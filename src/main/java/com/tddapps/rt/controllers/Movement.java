b0c633117d8ecc9715c45f9033bd0749a04cc58d
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 12:09:14 -0400

    no need to enable autoconfiguration

--- src/main/java/com/tddapps/rt/controllers/Movement.java
+++ src/main/java/com/tddapps/rt/controllers/Movement.java
@@ -1,11 +1,9 @@
 package com.tddapps.rt.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 public class Movement {
     @RequestMapping("/api/movement")
     String Get() {
