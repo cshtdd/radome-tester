c291d3016b6d7ff88cf95b94c1ca194e2056eca3
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 13:49:49 -0400

    add tests for the settings reader

---
+++ src/test/java/com/tddapps/model/internal/SettingsReaderEnvironmentTest.java
@@ -0,0 +1,27 @@
+package com.tddapps.model.internal;
+
+import com.tddapps.rt.model.SettingsReader;
+import com.tddapps.rt.model.internal.SettingsReaderEnvironment;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class SettingsReaderEnvironmentTest {
+    private final SettingsReader reader = new SettingsReaderEnvironment();
+
+    @Test
+    public void ReadsExistingEnvironmentSetting(){
+        var expected = System.getenv("PATH");
+
+        var actual = reader.Read("PATH", "");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ReturnsDefaultValueWhenSettingNotFound(){
+        var actual = reader.Read("SUPERCALIFRAGILISTIC", "mary");
+
+        assertEquals("mary", actual);
+    }
+}
