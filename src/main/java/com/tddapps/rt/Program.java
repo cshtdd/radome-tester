fd84a1c011569ecde92304d25d92a78b9dff4944
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 08:49:06 -0400

    create a very basic spring application

---
+++ src/main/java/com/tddapps/rt/Program.java
@@ -0,0 +1,10 @@
+package com.tddapps.rt;
+
+import com.tddapps.rt.controllers.Hello;
+import org.springframework.boot.SpringApplication;
+
+public class Program {
+    public static void main(String[] args){
+        SpringApplication.run(Hello.class, args);
+    }
+}
