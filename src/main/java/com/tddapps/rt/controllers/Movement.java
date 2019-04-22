b4345cf77ef55faa7a38e357f50c7f68304d012d
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 13:26:19 -0400

    install log4j

--- src/main/java/com/tddapps/rt/controllers/Movement.java
+++ src/main/java/com/tddapps/rt/controllers/Movement.java
@@ -1,12 +1,18 @@
 package com.tddapps.rt.controllers;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
+@Log4j2
 @RestController
 public class Movement {
     @RequestMapping("/api/movement")
     String Get() {
-        return "Movement World!";
+        var result = String.format("Hello World! %s", new Date().toInstant().toString());
+        log.info(result);
+        return result;
     }
 }
