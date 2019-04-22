66ecc2fcf97a3ca8eecf2a32765b3174b2cfc093
Camilo Sanchez <camilin87@gmail.com>
2019-04-22 11:56:46 -0400

    include static assets in the project

--- src/main/java/com/tddapps/rt/Program.java
+++ src/main/java/com/tddapps/rt/Program.java
@@ -1,10 +1,11 @@
 package com.tddapps.rt;
 
-import com.tddapps.rt.controllers.Hello;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Program {
     public static void main(String[] args){
-        SpringApplication.run(Hello.class, args);
+        SpringApplication.run(Program.class, args);
     }
 }
