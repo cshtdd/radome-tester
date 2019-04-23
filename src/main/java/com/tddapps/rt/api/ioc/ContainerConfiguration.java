bad9561cf2c4ff12aba1e12467361734f3a37f88
Camilo Sanchez <camilin87@gmail.com>
2019-04-23 07:47:39 -0400

    no need for the spring configuration to know how to resolve a settings reader

--- src/main/java/com/tddapps/rt/api/ioc/ContainerConfiguration.java
+++ src/main/java/com/tddapps/rt/api/ioc/ContainerConfiguration.java
@@ -1,18 +1,11 @@
 package com.tddapps.rt.api.ioc;
 
 import com.tddapps.rt.ioc.IocContainer;
-import com.tddapps.rt.model.SettingsReader;
-import com.tddapps.rt.model.internal.SettingsReaderEnvironment;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ContainerConfiguration {
-    @Bean
-    public SettingsReader settingsReader(){
-        return new SettingsReaderEnvironment();
-    }
-
     @Bean
     public IocContainer iocContainer() {
         return IocContainer.getInstance();
