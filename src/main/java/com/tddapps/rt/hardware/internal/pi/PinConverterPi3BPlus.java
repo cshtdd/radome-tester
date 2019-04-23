4b142e410aa355ffd7ba17c77268a73556d9f04e
Camilo Sanchez <camilin87@gmail.com>
2019-04-23 15:54:15 -0400

    create a BCMToGPIO pin converter

--- src/main/java/com/tddapps/rt/hardware/internal/pi/PinConverterPi3BPlus.java
+++ src/main/java/com/tddapps/rt/hardware/internal/pi/PinConverterPi3BPlus.java
@@ -4,8 +4,30 @@ import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.RaspiPin;
 import com.tddapps.rt.hardware.internal.PinConverter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PinConverterPi3BPlus implements PinConverter {
-    public Pin BCMToGPIO(int bcmPin){
-        return RaspiPin.GPIO_00;
+    private final Map<Integer, Pin> mappings = new HashMap<>(){{
+        put(17, RaspiPin.GPIO_00);
+        put(18, RaspiPin.GPIO_01);
+        put(27, RaspiPin.GPIO_02);
+        put(22, RaspiPin.GPIO_03);
+        put(23, RaspiPin.GPIO_04);
+        put(24, RaspiPin.GPIO_05);
+        put(25, RaspiPin.GPIO_06);
+        put(4, RaspiPin.GPIO_07);
+        put(28, RaspiPin.GPIO_08);
+        put(29, RaspiPin.GPIO_09);
+        put(30, RaspiPin.GPIO_10);
+        put(31, RaspiPin.GPIO_11);
+    }};
+
+    public Pin BCMToGPIO(int bcmPin) throws IllegalArgumentException {
+        if (!mappings.containsKey(bcmPin)){
+            throw new IllegalArgumentException(String.format("BcmPin out of range; bcmPin=%d", bcmPin));
+        }
+
+        return mappings.get(bcmPin);
     }
 }
--- src/test/java/com/tddapps/rt/hardware/internal/PinConverterTest.java
+++ src/test/java/com/tddapps/rt/hardware/internal/PinConverterTest.java
@@ -4,24 +4,58 @@ import com.pi4j.io.gpio.RaspiPin;
 import com.tddapps.rt.hardware.internal.pi.PinConverterPi3BPlus;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class PinConverterTest {
+    private final PinConverter converterPi3BPlus = new PinConverterPi3BPlus();
+
+    private void assertPi3BPlusConversionThrows(int bcmPin){
+        IllegalArgumentException throwException = null;
+
+        try {
+            converterPi3BPlus.BCMToGPIO(bcmPin);
+            fail(String.format("Should Have thrown for: %d", bcmPin));
+        } catch (IllegalArgumentException e){
+            throwException = e;
+        }
+
+        assertNotNull(throwException);
+    }
+
     @Test
     public void RaspberryPi3BPlus(){
-        var converter = new PinConverterPi3BPlus();
+        for (int i = -100; i <= 3; i++) {
+            assertPi3BPlusConversionThrows(i);
+        }
 
-        assertEquals(RaspiPin.GPIO_00, converter.BCMToGPIO(17));
-        assertEquals(RaspiPin.GPIO_01, converter.BCMToGPIO(18));
-        assertEquals(RaspiPin.GPIO_02, converter.BCMToGPIO(27));
-        assertEquals(RaspiPin.GPIO_03, converter.BCMToGPIO(22));
-        assertEquals(RaspiPin.GPIO_04, converter.BCMToGPIO(23));
-        assertEquals(RaspiPin.GPIO_05, converter.BCMToGPIO(24));
-        assertEquals(RaspiPin.GPIO_06, converter.BCMToGPIO(25));
-        assertEquals(RaspiPin.GPIO_07, converter.BCMToGPIO(4));
-        assertEquals(RaspiPin.GPIO_08, converter.BCMToGPIO(28));
-        assertEquals(RaspiPin.GPIO_09, converter.BCMToGPIO(29));
-        assertEquals(RaspiPin.GPIO_10, converter.BCMToGPIO(30));
-        assertEquals(RaspiPin.GPIO_11, converter.BCMToGPIO(31));
+        assertEquals(RaspiPin.GPIO_07, converterPi3BPlus.BCMToGPIO(4));
+
+        for (int i = 5; i <= 16; i++) {
+            assertPi3BPlusConversionThrows(i);
+        }
+
+        assertEquals(RaspiPin.GPIO_00, converterPi3BPlus.BCMToGPIO(17));
+        assertEquals(RaspiPin.GPIO_01, converterPi3BPlus.BCMToGPIO(18));
+
+        for (int i = 19; i <= 21; i++) {
+            assertPi3BPlusConversionThrows(i);
+        }
+
+        assertEquals(RaspiPin.GPIO_03, converterPi3BPlus.BCMToGPIO(22));
+        assertEquals(RaspiPin.GPIO_04, converterPi3BPlus.BCMToGPIO(23));
+        assertEquals(RaspiPin.GPIO_05, converterPi3BPlus.BCMToGPIO(24));
+        assertEquals(RaspiPin.GPIO_06, converterPi3BPlus.BCMToGPIO(25));
+
+        assertPi3BPlusConversionThrows(26);
+
+        assertEquals(RaspiPin.GPIO_02, converterPi3BPlus.BCMToGPIO(27));
+        assertEquals(RaspiPin.GPIO_08, converterPi3BPlus.BCMToGPIO(28));
+        assertEquals(RaspiPin.GPIO_09, converterPi3BPlus.BCMToGPIO(29));
+        assertEquals(RaspiPin.GPIO_10, converterPi3BPlus.BCMToGPIO(30));
+        assertEquals(RaspiPin.GPIO_11, converterPi3BPlus.BCMToGPIO(31));
+
+        for (int i = 32; i <= 100; i++) {
+            assertPi3BPlusConversionThrows(i);
+        }
     }
 }
