a434b2aa5356bf516323250ac39dcf81ba62cab8
Camilo Sanchez <camilin87@gmail.com>
2019-04-23 21:11:52 -0400

    create a very rudimentary client based page that posts position on demand

--- src/main/resources/public/js/send-position.js
+++ src/main/resources/public/js/send-position.js
@@ -10,6 +10,18 @@ export default function(jQuery){
                 var phi = jQuery(`${formId} #inputPhi`).val();
 
                 console.log(`Button Set Position Clicked; theta: ${theta}; phi: ${phi};`);
+
+                //TODO validate input values
+
+                jQuery.ajax('/api/movement', {
+                    data: JSON.stringify({ thetaDegrees: theta, phiDegrees: phi }),
+                    contentType: 'application/json',
+                    type: 'POST'
+                }).done(function(r){
+                    console.log('position sent; response: ', r);
+                }).fail(function(e) {
+                    console.log('transmission error: ', e);
+                });
             });
         }
     };
