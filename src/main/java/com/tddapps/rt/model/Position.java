d89bb01b7fd3da81958a3f623b6e38747239ef8c
Camilo Sanchez <camilin87@gmail.com>
2019-04-26 10:46:45 -0400

    minor Position validation refactoring

--- src/main/java/com/tddapps/rt/model/Position.java
+++ src/main/java/com/tddapps/rt/model/Position.java
@@ -4,11 +4,19 @@ import lombok.Data;
 
 @Data
 public class Position {
+    private static final double MIN_THETA = 180.0;
+    private static final double MAX_THETA = 360.0;
+
+    private static final double MIN_PHI = 0.0;
+    private static final double MAX_PHI = 180.0;
+
+
     private final double thetaDegrees;
     private final double phiDegrees;
 
     public boolean isValid() {
-        return (thetaDegrees >= 180 && thetaDegrees <= 360) &&
-                (phiDegrees >= 0 && phiDegrees <= 180);
+        var thetaIsValid = thetaDegrees >= MIN_THETA && thetaDegrees <= MAX_THETA;
+        var phiIsValid = phiDegrees >= MIN_PHI && phiDegrees <= MAX_PHI;
+        return thetaIsValid && phiIsValid;
     }
 }
