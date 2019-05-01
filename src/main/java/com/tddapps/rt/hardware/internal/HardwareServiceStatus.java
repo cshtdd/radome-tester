0db4609d969e3128a7cf74b78e678ffd489b954e
Camilo Sanchez <camilin87@gmail.com>
2019-05-01 08:30:55 -0400

    Start testing the HardwareServiceStatus

--- src/main/java/com/tddapps/rt/hardware/internal/HardwareServiceStatus.java
+++ src/main/java/com/tddapps/rt/hardware/internal/HardwareServiceStatus.java
@@ -3,9 +3,10 @@ package com.tddapps.rt.hardware.internal;
 import com.tddapps.rt.hardware.Delay;
 import com.tddapps.rt.hardware.HardwareService;
 import com.tddapps.rt.model.StatusRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class HardwareServiceStatus implements HardwareService {
-
     private final StatusRepository statusRepository;
     private final Delay delay;
 
@@ -17,5 +18,15 @@ public class HardwareServiceStatus implements HardwareService {
     @Override
     public void run() {
         //TODO finish this
+
+        while (RunCondition()){
+            // TODO send single movement steps
+
+            delay.Wait(1);
+        }
+    }
+
+    protected boolean RunCondition(){
+        return true;
     }
 }
--- src/test/java/com/tddapps/rt/hardware/internal/HardwareServiceStatusTest.java
+++ src/test/java/com/tddapps/rt/hardware/internal/HardwareServiceStatusTest.java
@@ -1,16 +1,44 @@
 package com.tddapps.rt.hardware.internal;
 
 import com.tddapps.rt.hardware.Delay;
-import com.tddapps.rt.hardware.HardwareService;
+import com.tddapps.rt.model.Status;
 import com.tddapps.rt.model.StatusRepository;
+import com.tddapps.rt.test.StatusRepositoryStub;
+import org.junit.Before;
+import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
 
 public class HardwareServiceStatusTest {
-    private final StatusRepository statusRepositoryMock = mock(StatusRepository.class);
-    private final Delay delay = new DelaySimulator();
+    private final StatusRepository statusRepository = new StatusRepositoryStub();
 
-    private final HardwareService service = new HardwareServiceStatus(statusRepositoryMock, delay);
+    private final HardwareServiceStatusTestable service = new HardwareServiceStatusTestable(
+            statusRepository, new DelaySimulator()
+    );
 
-    
+    @Before
+    public void Setup() {
+        statusRepository.Save(Status.builder().build());
+    }
+
+    private static class HardwareServiceStatusTestable extends HardwareServiceStatus {
+        public int MaxIterations = 10;
+        public int CurrentIteration = 0;
+
+        public HardwareServiceStatusTestable(StatusRepository statusRepository, Delay delay) {
+            super(statusRepository, delay);
+        }
+
+        @Override
+        protected boolean RunCondition() {
+            return CurrentIteration++ < MaxIterations;
+        }
+    }
+
+    @Test
+    public void RunFinishesBecauseItEvaluatesCondition() {
+        service.run();
+
+        assertEquals(11, service.CurrentIteration);
+    }
 }
