a21f8ca13245331f1cf3e5284412a8d188a9b1ca
Camilo Sanchez <camilin87@gmail.com>
2019-05-01 06:55:08 -0400

    Status has an isCalibrated property

--- src/main/java/com/tddapps/rt/model/Status.java
+++ src/main/java/com/tddapps/rt/model/Status.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @Builder(toBuilder = true)
 public class Status {
+    private boolean isCalibrated;
     private boolean isCalibrating;
     private boolean isMoving;
     private Position currentPosition;
