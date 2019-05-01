4aea344680b8ba7a07cd5b4a1862b1b3e1f36f33
Camilo Sanchez <camilin87@gmail.com>
2019-05-01 13:58:25 -0400

    Create a Settings class to hold the setting names constants

---
+++ src/main/java/com/tddapps/rt/config/Settings.java
@@ -0,0 +1,6 @@
+package com.tddapps.rt.config;
+
+public abstract class Settings {
+    public static final String THETA_BCM_PINS = "RT_THETA_BCM_PINS";
+    public static final String PHI_BCM_PINS = "RT_PHI_BCM_PINS";
+}
