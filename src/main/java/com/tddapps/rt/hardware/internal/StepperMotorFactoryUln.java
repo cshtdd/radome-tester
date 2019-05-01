053cec5f20a9bde5bea8882d20d5956ef385b3f8
Camilo Sanchez <camilin87@gmail.com>
2019-05-01 15:41:58 -0400

    Start defining how the StepperMotorFactory will behave

--- src/main/java/com/tddapps/rt/hardware/internal/StepperMotorFactoryUln.java
+++ src/main/java/com/tddapps/rt/hardware/internal/StepperMotorFactoryUln.java
@@ -1,4 +1,36 @@
 package com.tddapps.rt.hardware.internal;
 
-public class StepperMotorFactoryUln {
+import com.pi4j.io.gpio.GpioController;
+import com.tddapps.rt.config.ConfigurationReader;
+import com.tddapps.rt.hardware.StepperMotor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class StepperMotorFactoryUln implements StepperMotorFactory {
+    private final ConfigurationReader configurationReader;
+
+    private final Object criticalSection = new Object();
+
+    private GpioController gpio;
+    private StepperMotor motorTheta;
+    private StepperMotor motorPhi;
+
+
+    public StepperMotorFactoryUln(ConfigurationReader configurationReader) {
+        this.configurationReader = configurationReader;
+    }
+
+    @Override
+    public StepperMotor CreateTheta() {
+        synchronized (criticalSection){
+            return null;
+        }
+    }
+
+    @Override
+    public StepperMotor CreatePhi() {
+        synchronized (criticalSection){
+            return null;
+        }
+    }
 }
