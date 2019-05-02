3f443baef1bb37acd646e354b26f613ae7704fd1
Camilo Sanchez <camilin87@gmail.com>
2019-05-02 12:28:40 -0400

    HardwareServiceStatus creates the motors

--- src/main/java/com/tddapps/rt/hardware/internal/HardwareServiceStatus.java
+++ src/main/java/com/tddapps/rt/hardware/internal/HardwareServiceStatus.java
@@ -1,6 +1,7 @@
 package com.tddapps.rt.hardware.internal;
 
 import com.google.inject.Inject;
+import com.tddapps.rt.InvalidOperationException;
 import com.tddapps.rt.hardware.Delay;
 import com.tddapps.rt.hardware.HardwareService;
 import com.tddapps.rt.model.StatusRepository;
@@ -10,11 +11,16 @@ import lombok.extern.log4j.Log4j2;
 class HardwareServiceStatus implements HardwareService {
     private final StatusRepository statusRepository;
     private final Delay delay;
+    private final StepperMotorFactory stepperMotorFactory;
 
     @Inject
-    public HardwareServiceStatus(StatusRepository statusRepository, Delay delay) {
+    public HardwareServiceStatus(
+            StatusRepository statusRepository,
+            Delay delay,
+            StepperMotorFactory stepperMotorFactory) {
         this.statusRepository = statusRepository;
         this.delay = delay;
+        this.stepperMotorFactory = stepperMotorFactory;
     }
 
     @Override
@@ -28,9 +34,27 @@ class HardwareServiceStatus implements HardwareService {
             return;
         }
 
+        try {
+            //TODO get reference to the motor
+            stepperMotorFactory.CreateTheta();
+        } catch (InvalidOperationException e) {
+            //TODO log here
+            statusRepository.Save(initialStatus.toBuilder().isHardwareCrash(true).build());
+            return;
+        }
+
+        try {
+            stepperMotorFactory.CreatePhi();
+        } catch (InvalidOperationException e) {
+            //TODO log here
+            statusRepository.Save(initialStatus.toBuilder().isHardwareCrash(true).build());
+            return;
+        }
+
+        statusRepository.Save(initialStatus.toBuilder().isHardwareInitialized(true).build());
+
         while (RunCondition()){
             // TODO send single movement steps
-
             delay.Wait(1);
         }
     }
--- src/test/java/com/tddapps/rt/hardware/internal/HardwareServiceStatusTest.java
+++ src/test/java/com/tddapps/rt/hardware/internal/HardwareServiceStatusTest.java
@@ -1,5 +1,6 @@
 package com.tddapps.rt.hardware.internal;
 
+import com.tddapps.rt.InvalidOperationException;
 import com.tddapps.rt.hardware.Delay;
 import com.tddapps.rt.hardware.DelaySimulator;
 import com.tddapps.rt.model.Status;
@@ -8,13 +9,15 @@ import com.tddapps.rt.test.StatusRepositoryStub;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class HardwareServiceStatusTest {
     private final StatusRepository statusRepository = new StatusRepositoryStub();
+    private final StepperMotorFactory stepperMotorFactoryMock = mock(StepperMotorFactory.class);
 
     private final HardwareServiceStatusTestable service = new HardwareServiceStatusTestable(
-            statusRepository, new DelaySimulator()
+            statusRepository, new DelaySimulator(), stepperMotorFactoryMock
     );
 
     @Before
@@ -26,8 +29,11 @@ public class HardwareServiceStatusTest {
         public int MaxIterations = 1;
         public int CurrentIteration = 0;
 
-        public HardwareServiceStatusTestable(StatusRepository statusRepository, Delay delay) {
-            super(statusRepository, delay);
+        public HardwareServiceStatusTestable(
+                StatusRepository statusRepository,
+                Delay delay,
+                StepperMotorFactory stepperMotorFactory) {
+            super(statusRepository, delay, stepperMotorFactory);
         }
 
         @Override
@@ -52,5 +58,50 @@ public class HardwareServiceStatusTest {
         service.run();
 
         assertEquals(0, service.CurrentIteration);
+    }
+
+    @Test
+    public void CreatesThetaMotor() throws InvalidOperationException {
+        service.run();
+
+        verify(stepperMotorFactoryMock).CreateTheta();
+    }
+
+    @Test
+    public void ThetaMotorCreationFailureChangesStatus() throws InvalidOperationException {
+        when(stepperMotorFactoryMock.CreateTheta()).thenThrow(new InvalidOperationException());
+
+        service.run();
+
+        assertEquals(0, service.CurrentIteration);
+        assertFalse(statusRepository.CurrentStatus().isHardwareInitialized());
+        assertTrue(statusRepository.CurrentStatus().isHardwareCrash());
+    }
+
+    @Test
+    public void CreatesPhiMotor() throws InvalidOperationException {
+        service.run();
+
+        verify(stepperMotorFactoryMock).CreatePhi();
+    }
+
+    @Test
+    public void PhiMotorCreationFailureChangesStatus() throws InvalidOperationException {
+        when(stepperMotorFactoryMock.CreatePhi()).thenThrow(new InvalidOperationException());
+
+        service.run();
+
+        assertEquals(0, service.CurrentIteration);
+        assertFalse(statusRepository.CurrentStatus().isHardwareInitialized());
+        assertTrue(statusRepository.CurrentStatus().isHardwareCrash());
+    }
+
+    @Test
+    public void SetsHardwareInitializedStatus(){
+        service.run();
+
+        assertTrue(service.CurrentIteration > 0);
+        assertTrue(statusRepository.CurrentStatus().isHardwareInitialized());
+        assertFalse(statusRepository.CurrentStatus().isHardwareCrash());
     }
 }
