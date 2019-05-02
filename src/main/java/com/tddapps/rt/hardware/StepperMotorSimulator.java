4ec92f9ddc2f59577e112cc10b4dd2ce79a7f8f5
Camilo Sanchez <camilin87@gmail.com>
2019-05-02 12:41:47 -0400

    Create a StepperMotorSimulator

---
+++ src/main/java/com/tddapps/rt/hardware/StepperMotorSimulator.java
@@ -0,0 +1,39 @@
+package com.tddapps.rt.hardware;
+
+import com.tddapps.rt.InvalidOperationException;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class StepperMotorSimulator implements StepperMotor {
+    private final String name;
+
+    public StepperMotorSimulator(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void Init() {
+        log.info(ToLog("Init"));
+    }
+
+    @Override
+    public void Destroy() {
+        log.info(ToLog("Destroy"));
+    }
+
+    @Override
+    public boolean MoveCW() {
+        log.debug(ToLog("MoveCW"));
+        return true;
+    }
+
+    @Override
+    public boolean MoveCCW() throws InvalidOperationException {
+        log.debug(ToLog("MoveCCW"));
+        return true;
+    }
+
+    private String ToLog(String message){
+        return String.format("%s; motor: %s", message, name);
+    }
+}
