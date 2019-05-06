d3de24eddf82c59ab2a4e1add8a1a7a04d8f1339
Camilo Sanchez <camilin87@gmail.com>
2019-05-06 15:58:48 -0400

    Remove duplication in the Round class

--- src/main/java/com/tddapps/rt/utils/Round.java
+++ src/main/java/com/tddapps/rt/utils/Round.java
@@ -2,12 +2,16 @@ package com.tddapps.rt.utils;
 
 public abstract class Round {
     public static double PrecisionValue(int decimalPlaces){
-        return 1.0 / Math.pow(10.0, decimalPlaces);
+        return 1.0 / TenthPower(decimalPlaces);
     }
 
     public static double ToPrecision(double value, int decimalPlaces){
-        double tenthPower = Math.pow(10.0, decimalPlaces);
+        double tenthPower = TenthPower(decimalPlaces);
 
         return Math.round(value * tenthPower) / tenthPower;
+    }
+
+    private static double TenthPower(int decimalPlaces) {
+        return Math.pow(10.0, decimalPlaces);
     }
 }
