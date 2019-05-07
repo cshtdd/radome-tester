aabcc95ecee828ae546bfc17c965354c5815da1f
Camilo Sanchez <camilin87@gmail.com>
2019-05-07 10:28:18 -0400

    Change the CalibrationService so that it can throw exceptions

--- src/main/java/com/tddapps/rt/hardware/internal/CalibrationService.java
+++ src/main/java/com/tddapps/rt/hardware/internal/CalibrationService.java
@@ -1,8 +1,9 @@
 package com.tddapps.rt.hardware.internal;
 
+import com.tddapps.rt.InvalidOperationException;
 import com.tddapps.rt.hardware.StepperMotor;
 
 interface CalibrationService {
-    void CalibrateThetaStepper(StepperMotor motor);
-    void CalibratePhiStepper(StepperMotor motor);
+    void CalibrateThetaStepper(StepperMotor motor) throws InvalidOperationException;
+    void CalibratePhiStepper(StepperMotor motor) throws InvalidOperationException;
 }
