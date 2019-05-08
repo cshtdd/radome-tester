fdbfbbbd4a8faab97a4a6a6f1f2ac64da4017778
Camilo Sanchez <camilin87@gmail.com>
2019-05-08 08:43:31 -0400

    Add sensible logging to the StepperMotorMoverStatus

--- src/main/java/com/tddapps/rt/hardware/internal/StepperMotorMoverStatus.java
+++ src/main/java/com/tddapps/rt/hardware/internal/StepperMotorMoverStatus.java
@@ -5,8 +5,13 @@ import com.tddapps.rt.hardware.Direction;
 import com.tddapps.rt.hardware.StepperMotor;
 import com.tddapps.rt.model.Position;
 import com.tddapps.rt.model.StatusRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class StepperMotorMoverStatus implements StepperMotorMover {
+    private final static String THETA = "Theta";
+    private final static String PHI = "Phi";
+
     private final StatusRepository statusRepository;
     private final MovementCalculator movementCalculator;
     private final StepperPrecisionRepository stepperPrecisionRepository;
@@ -26,36 +31,29 @@ public class StepperMotorMoverStatus implements StepperMotorMover {
         var currentStatus = statusRepository.CurrentStatus();
 
         if (!currentStatus.isMoving()){
-            // TODO, maybe log something
             return;
         }
+
+        log.info(String.format("Movement Started; axis=%s;", THETA));
 
         var src = currentStatus.getCurrentPosition();
         var dest = currentStatus.getCommandedPosition();
 
         if (!src.IsValid()){
-            // TODO, log something
-            var updatedStatus = statusRepository.CurrentStatus()
-                    .toBuilder()
-                    .isMoving(false)
-                    .build();
-            statusRepository.Save(updatedStatus);
+            HaltMovement("Invalid Current Position");
             return;
         }
 
         if (!dest.IsValid()){
-            // TODO, log something
-            var updatedStatus = statusRepository.CurrentStatus()
-                    .toBuilder()
-                    .isMoving(false)
-                    .build();
-            statusRepository.Save(updatedStatus);
+            HaltMovement("Invalid Commanded Position");
             return;
         }
 
         var precision = stepperPrecisionRepository.ReadTheta();
         var direction = movementCalculator.CalculateThetaDirection(src, dest);
         var steps = movementCalculator.CalculateThetaSteps(src, dest, precision);
+
+        log.info(String.format("Moving motor; axis=%s; direction=%s; steps=%d;", THETA, direction, steps));
 
         for (int i = 0; i < steps; i++) {
             Move(motor, direction);
@@ -64,13 +62,7 @@ public class StepperMotorMoverStatus implements StepperMotorMover {
         var updatedPosition = src.toBuilder()
                 .thetaDegrees(dest.getThetaDegrees())
                 .build();
-        var isMoving = !updatedPosition.AlmostEquals(dest);
-        var updatedStatus = statusRepository.CurrentStatus()
-                .toBuilder()
-                .currentPosition(updatedPosition)
-                .isMoving(isMoving)
-                .build();
-        statusRepository.Save(updatedStatus);
+        CompleteMovement(THETA, updatedPosition, dest);
     }
 
     @Override
@@ -86,12 +78,30 @@ public class StepperMotorMoverStatus implements StepperMotorMover {
                 motor.MoveCCW();
             }
         } catch (InvalidOperationException e){
-            var updatedStatus = statusRepository.CurrentStatus()
-                    .toBuilder()
-                    .isMoving(false)
-                    .build();
-            statusRepository.Save(updatedStatus);
+            HaltMovement("Movement Error");
             throw e;
         }
+    }
+
+    private void HaltMovement(String reason) {
+        log.info(String.format("Stop Movement; reason=%s", reason));
+        var updatedStatus = statusRepository.CurrentStatus()
+                .toBuilder()
+                .isMoving(false)
+                .build();
+        statusRepository.Save(updatedStatus);
+    }
+
+    private void CompleteMovement(String axis, Position updatedPosition, Position commandedPosition) {
+        var isStillMoving = !updatedPosition.AlmostEquals(commandedPosition);
+
+        log.info(String.format("Movement Completed; axis=%s; isMoving=%s", axis, isStillMoving));
+
+        var updatedStatus = statusRepository.CurrentStatus()
+                .toBuilder()
+                .currentPosition(updatedPosition)
+                .isMoving(isStillMoving)
+                .build();
+        statusRepository.Save(updatedStatus);
     }
 }
