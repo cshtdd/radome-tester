be8a10b3019d98b6fa4bee82d2dd7ea7c9f4b701
Camilo Sanchez <camilin87@gmail.com>
2019-05-16 12:32:52 -0400

    Inline the mappers to avoid their horrible names

--- src/main/java/com/tddapps/rt/mapping/internal/AutoMapper.java
+++ src/main/java/com/tddapps/rt/mapping/internal/AutoMapper.java
@@ -5,7 +5,9 @@ import com.tddapps.rt.api.controllers.StatusController;
 import com.tddapps.rt.mapping.Mapper;
 import com.tddapps.rt.model.Position;
 import com.tddapps.rt.model.Status;
+import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import ma.glasnost.orika.metadata.TypeFactory;
 
@@ -17,11 +19,42 @@ class AutoMapper implements Mapper {
 
         mapperFactory.classMap(Status.class, StatusController.StatusResponse.class)
                 .byDefault()
-                .customize(new StatusToStatusControllerStatusResponse())
+                .customize(new CustomMapper<Status, StatusController.StatusResponse>() {
+                    @Override
+                    public void mapAtoB(Status status, StatusController.StatusResponse statusResponse, MappingContext context) {
+                        super.mapAtoB(status, statusResponse, context);
+
+                        statusResponse.setIsCalibrating(status.isCalibrating());
+                        statusResponse.setIsCalibrated(status.isCalibrated());
+                        statusResponse.setIsHardwareInitialized(status.isHardwareInitialized());
+                        statusResponse.setIsHardwareCrash(status.isHardwareCrash());
+                        statusResponse.setIsMoving(status.isMoving());
+                        statusResponse.setIsPanning(status.isPanning());
+
+                        var currentPosition = status.getCurrentPosition();
+                        if (currentPosition != null){
+                            statusResponse.setTheta(currentPosition.getThetaDegrees());
+                            statusResponse.setPhi(currentPosition.getPhiDegrees());
+                        }
+
+                        var commandedPosition = status.getCommandedPosition();
+                        if (commandedPosition != null){
+                            statusResponse.setCommandedTheta(commandedPosition.getThetaDegrees());
+                            statusResponse.setCommandedPhi(commandedPosition.getPhiDegrees());
+                        }
+                    }
+                })
                 .register();
 
         mapperFactory.registerObjectFactory(
-                new MovementControllerMovementStartRequestToPosition(),
+                (source, mappingContext) -> {
+                    var movementRequest = (MovementController.MovementStartRequest) source;
+
+                    var theta = movementRequest.getTheta();
+                    var phi = movementRequest.getPhi();
+
+                    return new Position(theta, phi);
+                },
                 TypeFactory.valueOf(Position.class),
                 TypeFactory.valueOf(MovementController.MovementStartRequest.class));
     }
