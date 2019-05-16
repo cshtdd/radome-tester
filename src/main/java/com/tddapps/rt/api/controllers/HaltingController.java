c0152dd4a927c5af50ae19eec704aff57e9f6530
Camilo Sanchez <camilin87@gmail.com>
2019-05-16 12:15:30 -0400

    Create a HaltingController

---
+++ src/main/java/com/tddapps/rt/api/controllers/HaltingController.java
@@ -0,0 +1,35 @@
+package com.tddapps.rt.api.controllers;
+
+import com.tddapps.rt.ioc.IocContainer;
+import com.tddapps.rt.model.MovementService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+public class HaltingController {
+    private final IocContainer container;
+
+    public HaltingController(IocContainer container) {
+        this.container = container;
+    }
+
+    @PostMapping(value = "/api/halting")
+    ResponseEntity<String> Post() {
+        var movementService = container.Resolve(MovementService.class);
+
+        log.info("Halting;");
+        try {
+            movementService.Stop();
+        } catch (Exception e) {
+            log.error("Halting Error; reason: Unexpected; details:", e);
+            return new ResponseEntity<>("Unexpected Error", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        log.info("Halting Started");
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
+}
