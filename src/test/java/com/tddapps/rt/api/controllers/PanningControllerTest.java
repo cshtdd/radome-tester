cade793ecb0beb5d8c44bd8636cfc7a9bbcd2b2a
Camilo Sanchez <camilin87@gmail.com>
2019-05-16 12:01:23 -0400

    The PanningController works as expected

--- src/test/java/com/tddapps/rt/api/controllers/PanningControllerTest.java
+++ src/test/java/com/tddapps/rt/api/controllers/PanningControllerTest.java
@@ -1,5 +1,6 @@
 package com.tddapps.rt.api.controllers;
 
+import com.tddapps.rt.InvalidOperationException;
 import com.tddapps.rt.ioc.IocContainer;
 import com.tddapps.rt.model.MovementService;
 import org.junit.Before;
@@ -14,8 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static com.tddapps.rt.mapping.internal.IocContainerAutoMappingHelper.SetupAutoMapping;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,20 +37,51 @@ public class PanningControllerTest {
         when(containerMock.Resolve(MovementService.class)).thenReturn(movementServiceMock);
     }
 
-    private ResultActions Post(String jsonBody) throws Exception {
+    private ResultActions Post() throws Exception {
         var request = post("/api/panning")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonBody);
+                .contentType(MediaType.APPLICATION_JSON);
         return web.perform(request);
     }
 
     @Test
     public void Returns400WhenCannotPan() throws Exception {
-        when(movementServiceMock.CanPan())
-                .thenReturn(false);
+        when(movementServiceMock.CanPan()).thenReturn(false);
 
-        Post("{}")
+        Post()
                 .andExpect(status().is4xxClientError())
                 .andExpect(content().string("Cannot Pan"));
+    }
+
+    @Test
+    public void Returns400WhenPanningIsInvalid() throws Exception {
+        when(movementServiceMock.CanPan()).thenReturn(true);
+        doThrow(new InvalidOperationException()).when(movementServiceMock).Pan();
+
+        Post()
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string("Panning Error"));
+    }
+
+    @Test
+    public void Returns500WhenMovementFails() throws Exception {
+        when(movementServiceMock.CanPan()).thenReturn(true);
+        doAnswer(i -> {
+            throw new Exception();
+        }).when(movementServiceMock).Pan();
+
+        Post()
+                .andExpect(status().is5xxServerError())
+                .andExpect(content().string("Unexpected Error"));
+    }
+
+    @Test
+    public void ReturnsOkWhenPanningSucceeds() throws Exception {
+        when(movementServiceMock.CanPan()).thenReturn(true);
+
+        Post()
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+
+        verify(movementServiceMock).Pan();
     }
 }
