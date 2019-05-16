ca6916b96c9b97d9afc9a806f9b3dd36e3e2419f
Camilo Sanchez <camilin87@gmail.com>
2019-05-16 14:27:04 -0400

    Refactor the HardwareInitializer tests to allow extensibility of other Daemons

--- src/test/java/com/tddapps/rt/hardware/HardwareInitializerTest.java
+++ src/test/java/com/tddapps/rt/hardware/HardwareInitializerTest.java
@@ -4,24 +4,27 @@ import com.tddapps.rt.StartupService;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.tddapps.rt.test.ConditionAwaiter.Await;
 import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class HardwareInitializerTest {
-    private boolean runInvoked = false;
+    private List<String> runDaemons = new ArrayList<>();
     private final MovementDaemon movementDaemonMock = mock(MovementDaemon.class);
 
     private final StartupService service = new HardwareInitializer(movementDaemonMock);
 
-    private boolean RunWasInvoked(){
-        return runInvoked;
+    private boolean RunMovementWasInvoked(){
+        return runDaemons.contains("Movement");
     }
 
     @Before
     public void Setup(){
         doAnswer(i -> {
-            runInvoked = true;
+            runDaemons.add("Movement");
             return null;
         }).when(movementDaemonMock).run();
     }
@@ -30,6 +33,6 @@ public class HardwareInitializerTest {
     public void RunsTheHardwareService() {
         service.RunAsync(new String[]{});
 
-        assertTrue(Await(this::RunWasInvoked));
+        assertTrue(Await(this::RunMovementWasInvoked));
     }
 }
