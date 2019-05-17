6da6096dcb9fff94e097f964242ccd9d68bc33e1
Camilo Sanchez <camilin87@gmail.com>
2019-05-17 12:19:11 -0400

    Create the PanningPrecision setting that will be used to read an environment variable

--- src/main/java/com/tddapps/rt/config/Settings.java
+++ src/main/java/com/tddapps/rt/config/Settings.java
@@ -4,4 +4,5 @@ public abstract class Settings {
     public static final String THETA_BCM_PINS = "RT_THETA_BCM_PINS";
     public static final String PHI_BCM_PINS = "RT_PHI_BCM_PINS";
     public static final String IS_SIMULATION = "RT_IS_SIMULATION";
+    public static final String PANNING_PRECISION = "RT_PANNING_PRECISION";
 }
