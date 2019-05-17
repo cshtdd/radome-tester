bea22972a863a9744b70380f0bea2df22f54d908
Camilo Sanchez <camilin87@gmail.com>
2019-05-17 12:04:43 -0400

    Panning increments will be larger during simulations

--- src/main/java/com/tddapps/rt/model/internal/PanningSettingsRepositoryFixed.java
+++ src/main/java/com/tddapps/rt/model/internal/PanningSettingsRepositoryFixed.java
@@ -1,13 +1,30 @@
 package com.tddapps.rt.model.internal;
 
+import com.google.inject.Inject;
+import com.tddapps.rt.config.ConfigurationReader;
 import com.tddapps.rt.model.Position;
 
 public class PanningSettingsRepositoryFixed implements PanningSettingsRepository {
+    private final ConfigurationReader configurationReader;
+
+    @Inject
+    public PanningSettingsRepositoryFixed(ConfigurationReader configurationReader) {
+        this.configurationReader = configurationReader;
+    }
+
     @Override
     public PanningSettings Read() {
         return new PanningSettings(
-                Position.MIN_THETA, Position.MAX_THETA, 0.1,
-                Position.MIN_PHI, Position.MAX_PHI, 0.1
+                Position.MIN_THETA, Position.MAX_THETA, StepsIncrement(),
+                Position.MIN_PHI, Position.MAX_PHI, StepsIncrement()
         );
+    }
+
+    private double StepsIncrement(){
+        if (configurationReader.Read().isSimulation()){
+            return 1.0;
+        }
+
+        return 0.1;
     }
 }
--- src/test/java/com/tddapps/rt/model/internal/PanningSettingsRepositoryFixedTest.java
+++ src/test/java/com/tddapps/rt/model/internal/PanningSettingsRepositoryFixedTest.java
@@ -1,17 +1,43 @@
 package com.tddapps.rt.model.internal;
 
+import com.tddapps.rt.config.Configuration;
+import com.tddapps.rt.config.ConfigurationReader;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PanningSettingsRepositoryFixedTest {
-    private final PanningSettingsRepository repository = new PanningSettingsRepositoryFixed();
+    private Configuration configuration = new Configuration();
+
+    private final ConfigurationReader configurationReaderMock = mock(ConfigurationReader.class);
+    private final PanningSettingsRepository repository = new PanningSettingsRepositoryFixed(configurationReaderMock);
+
+    @Before
+    public void Setup(){
+        when(configurationReaderMock.Read()).thenReturn(configuration);
+    }
 
     @Test
     public void ReturnsTheCorrectSettings(){
         var expected = new PanningSettings(
                 180, 360, 0.1,
                 0, 180, 0.1
+        );
+
+        var actual = repository.Read();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ReturnsTheSimulationSettings(){
+        configuration.setSimulation(true);
+        var expected = new PanningSettings(
+                180, 360, 1,
+                0, 180, 1
         );
 
         var actual = repository.Read();
